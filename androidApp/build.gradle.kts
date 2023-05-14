3b8f6f44e8cf3439aa9f91b59d79df780aa737d4
Ramit Suri <ramitsuri007@gmail.com>
2023-05-14 08:46:16 -0400

    Release 0.17(17)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "com.ramitsuri.expensereports.android"
         minSdk = 28
         targetSdk = 33
-        versionCode = 16
-        versionName = "0.16"
+        versionCode = 17
+        versionName = "0.17"
     }
     packagingOptions {
         resources.excludes.add("META-INF/*.kotlin_module")
