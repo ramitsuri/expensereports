87962a3fd668f304b90be21a4966331c27f897fa
Ramit Suri <ramitsuri007@gmail.com>
2023-10-22 20:19:35 -0400

    Release 2.4

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ramitsuri.expensereports.android"
         minSdk = 28
         targetSdk = 34
-        versionCode = 23
-        versionName = "2.3"
+        versionCode = 24
+        versionName = "2.4"
     }
     packagingOptions {
         resources.excludes.add("META-INF/*.kotlin_module")
