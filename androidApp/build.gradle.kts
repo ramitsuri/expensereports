d75f925ba7a6cc0db6bcd9d896c128280c7d2a0e
Ramit Suri <ramitsuri007@gmail.com>
2024-01-10 07:12:32 -0500

    Release 3.2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ramitsuri.expensereports.android"
         minSdk = 28
         targetSdk = 34
-        versionCode = 31
-        versionName = "3.1"
+        versionCode = 32
+        versionName = "3.2"
     }
     packagingOptions {
         resources.excludes.add("META-INF/*.kotlin_module")
