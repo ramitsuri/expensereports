3ab2fdd7c4e70f29de4f93fc1db0e1ffb1359367
Ramit Suri <ramitsuri007@gmail.com>
2025-03-29 22:44:34 -0400

    Remove old code

--- androidApp/src/main/java/com/ramitsuri/expensereports/android/ui/home/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.ramitsuri.expensereports.android.ui.home
-
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HomeScreen(
-) {
-}
--- androidApp/src/main/java/com/ramitsuri/expensereports/android/ui/transactions/TransactionsScreen.kt
+++
@@ -1,6 +0,0 @@
-package com.ramitsuri.expensereports.android.ui.transactions
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun TransactionsScreen(){}
